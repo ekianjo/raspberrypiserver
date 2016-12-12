d17b00aa488d19f8237e9007dc24f117d3070e1c
ekianjo <ekianjo@gmail.com>
2016-12-12 10:31:48 +0900

    new file

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo apt-get update && sudo apt-get upgrade
+sudo apt-get install vim ncdu weechat-curses apache2 fail2ban htop tmux glances aria2 youtube-dl python-pip python3 python3-pip task surfraw mutt ffmpeg netcat iftop mtr newsbeuter
+sudo pip install mps
+sudo pip install haxor-news
+sudo pip3 install hangups
+
